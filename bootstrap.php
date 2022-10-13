563fa973e88619c1339f93784ddbb1d4c4f3f7b0
Nilambar Sharma <nilambar@outlook.com>
2022-10-13 15:38:56 +0545

    fix: boot

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
-function wp_welcome_bootstrap() {
-	if ( is_admin() ) {
-		do_action( 'wp_welcome_init' );
+if ( ! function_exists( 'wp_welcome_bootstrap' ) ) {
+	function wp_welcome_bootstrap() {
+		if ( is_admin() ) {
+			do_action( 'wp_welcome_init' );
+		}
 	}
 }
